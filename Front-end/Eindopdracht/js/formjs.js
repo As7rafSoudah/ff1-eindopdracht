3d814ec81bef865b6fd7a529d480211bb9489f35
As7RaF <57992842+As7rafSoudah@users.noreply.github.com>
2020-04-11 16:56:20 +0200

    Dit is mijn huiswerk(Ashraf Soudah)

---
+++ Front-end/Eindopdracht/js/formjs.js
@@ -0,0 +1,6 @@
+function MyFunction(){
+    // Selecting the input element and get its value 
+    var input = document.getElementById("super").value;
+    // Displaying the value
+    alert(input);
+}
